0414a97a00fc025fe8202af0a9fc1f31aa70440e
Elmo <113604229+elfilibusterismo@users.noreply.github.com>
2024-08-30 05:44:23 +0800

    feat: Enhance Telegram notification with Play Store link
    Added a "View App on Play Store" link to the Telegram notification message. The script now includes a direct link to the app on the Play Store, along with the commit details.

--- send_telegram.py
+++ send_telegram.py
@@ -17,11 +17,14 @@ def send_message(chat_id, token, message, topic_id=None):
 if __name__ == "__main__":
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
     TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+    COMMIT_AUTHOR = os.getenv('GITHUB_ACTOR')
     COMMIT_MESSAGE = os.getenv('COMMIT_MESSAGE')
     TELEGRAM_TOPIC_ID = os.getenv('TELEGRAM_TOPIC_ID', None)
+    PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.elfilibustero.origin"
 
-    message = f"New commit in `{os.getenv('GITHUB_REPOSITORY')}`:\n\n{COMMIT_MESSAGE}"
-    
+    message = f"*Author:* {COMMIT_AUTHOR}\n" \
+                f"{COMMIT_MESSAGE}\n\n" \
+                f"🚀 [View App on Play Store]({PLAYSTORE_URL})"
     response = send_message(TELEGRAM_CHAT_ID, TELEGRAM_TOKEN, message, TELEGRAM_TOPIC_ID)
     if response.status_code != 200:
         raise Exception(f"Failed to send message: {response.text}")
