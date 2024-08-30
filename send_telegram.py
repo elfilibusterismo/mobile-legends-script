13e7a7e8c6d730374db0c005786fec50796f65e7
elfilibusterismo <elfilibusterismo.contact@gmail.com>
2024-08-31 01:44:02 +0800

    feat: Update workflow to send telegram all the commit messages

--- send_telegram.py
+++ send_telegram.py
@@ -18,12 +18,14 @@ if __name__ == "__main__":
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
     TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
     COMMIT_AUTHOR = os.getenv('GITHUB_ACTOR')
-    COMMIT_MESSAGE = os.getenv('COMMIT_MESSAGE')
     TELEGRAM_TOPIC_ID = os.getenv('TELEGRAM_TOPIC_ID', None)
     PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.elfilibustero.origin"
+    
+    with open('commit_messages.txt', 'r') as file:
+    commit_messages = file.read().replace(' ', '\n')
 
     message = f"*Author:* {COMMIT_AUTHOR}\n" \
-                f"{COMMIT_MESSAGE}\n\n" \
+                f"{commit_messages}\n\n" \
                 f"🚀 [View App on Play Store]({PLAYSTORE_URL})"
     response = send_message(TELEGRAM_CHAT_ID, TELEGRAM_TOKEN, message, TELEGRAM_TOPIC_ID)
     if response.status_code != 200:
